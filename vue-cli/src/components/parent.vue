bb3c8c9db33152b588d11311075eeedbcf62568e
jing <244457187@qq.com>
2018-09-27 11:34:11 +0800

    父子通信

---
+++ vue-cli/src/components/parent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <span>我是爸爸</span>
+    <hr>
+    <son :msg = "toSonMsg"></son>
+  </div>
+
+</template>
+
+<script>
+  import son from "./son"
+
+  export default {
+    name: "parent",
+    components: {
+      son,
+    },
+    data: function () {
+      return {
+        toSonMsg: '我是你爸传过来的'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-cli/src/components/son.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <span>我是儿子</span>
+      <br>
+      爸爸传来的数据---{{msg}}
+      <button>向爸爸发送数据</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "son",
+        props: {
+          msg: {
+            type: String,
+            default: ''
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
